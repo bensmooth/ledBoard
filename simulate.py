786cce4b2d5bce44040b6e97636b24c8e749999f
Ben Hart <bensmooth@gmail.com>
2016-02-23 00:59:59 -0600

    Split some event handling logic into the scene class.

--- simulate.py
+++ simulate.py
@@ -23,47 +23,53 @@ def GetPixelAtScreenCoordinates(xy):
 	yCoord = xy[1] / (PIXEL_SIZE + BORDER_SIZE) 
 	return (xCoord, yCoord)
 
-def ColorPixelAtScreenCoordinates(imageData, screenCoordinates, color):
-	pixelToColor = GetPixelAtScreenCoordinates(screenCoordinates)
-	modifiedPixelIndex = CoordinatesToIndex(pixelToColor[0], pixelToColor[1])
-	imageData[modifiedPixelIndex] = FormatPixel(color, pixelToColor)
-
 class DrawingScene:
-	def __init__(self, backgroundColor, initialBrushColor):
+	def __init__(self, initialImageData, backgroundColor, initialBrushColor):
+		self.imageData = initialImageData
 		self.fontRenderer = pygame.font.SysFont("monospace", 42)
 		self.backgroundColor = backgroundColor
 		self.brushColor = initialBrushColor
+		self.drawableGridArea = DISPLAY_WIDTH * (PIXEL_SIZE + BORDER_SIZE), DISPLAY_HEIGHT * (PIXEL_SIZE + BORDER_SIZE)
+		self.totalArea = self.drawableGridArea[0], self.drawableGridArea[1] + COLOR_PICKER_HEIGHT
 
-	def OnLoop(self, screen, imageData):
+	def OnLoop(self, screen):
 		screen.fill(self.backgroundColor)
 
 		colorText = "({}, {}, {})".format(*self.brushColor)
 		colorTextSurface = self.fontRenderer.render(colorText, True, (230, 230, 230), self.backgroundColor)
 		screen.blit(colorTextSurface, (0, screen.get_height() - colorTextSurface.get_height()))
-		self.DrawImage(screen, imageData)
+		self.DrawImage(screen)
 
-		pygame.display.flip()
+	def HandleMouseClick(self, clickCoordinates):
+		# See if this is even in the area that we care about.
+		areaWeCareAbout = pygame.Rect((0, 0), self.drawableGridArea)
+		if not areaWeCareAbout.collidepoint(clickCoordinates):
+			return
+
+		# Color the pixel that was clicked on.
+		pixelToColor = GetPixelAtScreenCoordinates(clickCoordinates)
+		modifiedPixelIndex = CoordinatesToIndex(pixelToColor[0], pixelToColor[1])
+		self.imageData[modifiedPixelIndex] = FormatPixel(self.brushColor, pixelToColor)
 
 	def DrawPixelAtIndex(self, surface, color, index):
 		coords = IndexToCoordinates(index)
 		DrawPixel(surface, color, coords[0], coords[1])
 
-	def DrawImage(self, surface, imageData):
-		for index, pixel in enumerate(imageData):
+	def DrawImage(self, surface):
+		for index, pixel in enumerate(self.imageData):
 			self.DrawPixelAtIndex(surface, pixel, index)
 
 
 def main(argv):
 	assert (len(argv) == 2), "Usage: {} <image filename>".format(argv[0])
-	pygame.init()
 	filename = argv[1]
+	pygame.init()
 
-	windowSize = DISPLAY_WIDTH * (PIXEL_SIZE + BORDER_SIZE), DISPLAY_HEIGHT * (PIXEL_SIZE + BORDER_SIZE) + COLOR_PICKER_HEIGHT
-	drawingScene = DrawingScene(pygame.Color("Black"), pygame.Color("White"))
-	screen = pygame.display.set_mode(windowSize)
+	initialImageData = LoadPixData(filename)
+	drawingScene = DrawingScene(initialImageData, pygame.Color("Black"), pygame.Color("Black"))
 
-	imageData = LoadPixData(filename)
 	clock = pygame.time.Clock()
+	screen = pygame.display.set_mode(drawingScene.totalArea)
 
 	while 1:
 		for event in pygame.event.get():
@@ -71,15 +77,21 @@ def main(argv):
 				keys = pygame.key.get_pressed()
 				if keys[pygame.K_ESCAPE]:
 					sys.exit()
-				elif keys[pygame.K_LCTRL] and keys[pygame.K_s]:
-					print("Saving to {}".format(filename))
-					WritePixData(filename, imageData)
+				elif keys[pygame.K_LCTRL]:
+					if keys[pygame.K_s]:
+						print("Saving to {}".format(filename))
+						WritePixData(filename, drawingScene.imageData)
+				else:
+					# TODO: Handle color picker typing here.
+					print("Pressed a key")
 			if event.type == pygame.QUIT:
 				sys.exit()
 			if (event.type == pygame.MOUSEMOTION and event.buttons[0] == 1) or (event.type == pygame.MOUSEBUTTONDOWN and event.button == 1):
-				ColorPixelAtScreenCoordinates(imageData, event.pos, drawingScene.brushColor)
+				drawingScene.HandleMouseClick(event.pos)
 
-		drawingScene.OnLoop(screen, imageData)
+		drawingScene.OnLoop(screen)
+
+		pygame.display.flip()
 
 		clock.tick(30)
 
