041b89314cce7d131389da97abdd1c92866eac51
Ben Hart <bensmooth@gmail.com>
2016-07-29 23:28:48 -0500

    Added Conways game of life.

--- cppPixLib/Color.h
+++ cppPixLib/Color.h
@@ -4,7 +4,6 @@
 
 namespace Pix
 {
-
 	class Color
 	{
 	public:
@@ -24,9 +23,21 @@ namespace Pix
 		const uint8_t& Green() const;
 		const uint8_t& Blue() const;
 
+		friend bool operator==(const Color& lhs, const Color& rhs);
+
 	private:
 		uint8_t m_r;
 		uint8_t m_g;
 		uint8_t m_b;
 	};
+
+	inline bool operator==(const Color& lhs, const Color& rhs)
+	{
+		return (lhs.m_r == rhs.m_r && lhs.m_g == rhs.m_g && lhs.m_b == rhs.m_b);
+	}
+
+	inline bool operator!=(const Color& lhs, const Color& rhs)
+	{
+		return !operator==(lhs, rhs);
+	}
 }
